68bbfe99d0b15a212679b89463f6ca44318cdc80
ldelgado <ldelgado@SC-SI-LDELGADO>
2025-07-31 21:13:07 -0600

    Add project files.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PRUEBA1.Models;
+
+namespace PRUEBA1.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly Cliente2025Context _context;
+
+        public ClientesController(Cliente2025Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Clientes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clientes = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.pkclientes == id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return View(clientes);
+        }
+
+        // GET: Clientes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("pkclientes,nombre,apellido,cedula")] Clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(clientes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(clientes);
+        }       
+        
+        public async Task<IActionResult> CreateAJAX([FromBody] Clientes cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Add(cliente);
+                await _context.SaveChangesAsync();
+                return Json("éxito");
+           
+            }
+            return Json("falso");
+
+        }      
+        
+        public async Task<IActionResult> CargarInfo()
+        {
+            var clientes = await _context.Clientes.ToListAsync();
+
+            return Json(clientes);                
+
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clientes = await _context.Clientes.FindAsync(id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("pkclientes,nombre,apellido,cedula")] Clientes clientes)
+        {
+            if (id != clientes.pkclientes)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(clientes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientesExists(clientes.pkclientes))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(clientes);
+        }
+
+        // GET: Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clientes = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.pkclientes == id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return View(clientes);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var clientes = await _context.Clientes.FindAsync(id);
+            if (clientes != null)
+            {
+                _context.Clientes.Remove(clientes);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientesExists(int id)
+        {
+            return _context.Clientes.Any(e => e.pkclientes == id);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PRUEBA1.Models;
+
+namespace PRUEBA1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }  
+        public IActionResult Prueba()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
